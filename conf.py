dbc7e462402f52ae912096c0fe5a59c953fd19e7
Sebastian Rahlf <basti@redtoad.de>
2015-06-22 23:19:19 +0200

    Add archive link in sidebar.

--- conf.py
+++ conf.py
@@ -89,7 +89,7 @@ exclude_patterns = ['drafts/*', '_templates/*']
 
 # Add templates to be rendered in sidebar here
 html_sidebars = {
-    '**': ['recent.html', 'searchbox.html']
+    '**': ['recent.html', 'searchbox.html', 'archive_link_sidebar.html']
 }
 
 # Add an index to the HTML documents.
